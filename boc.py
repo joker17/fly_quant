8431b0dbd679c4d25714187b194a5dc2f159253e
hj.sinp <openout5@qq.com>
2017-10-25 22:04:06 +0800

    commit

---
+++ boc.py
@@ -0,0 +1,10 @@
+def initialize(context):
+    g.security = '600570.SS'
+    set_universe([g.security])
+
+def handle_data(context, data):
+    if g.security not in context.portfolio.positions:
+        order(g.security, 1000)
+    else:
+        order(g.security, -800)
+    pass
